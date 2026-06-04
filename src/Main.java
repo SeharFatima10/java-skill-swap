5b06b020529711a6c906ce9086fcfa8622157cea
Sehar Fatima <sehrfatima2222@gmail.com>
2026-06-04 11:51:38 +0500

    Added Java skill swap project

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,91 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+class Student {
+    int id;
+    String name;
+    ArrayList<String> skills;
+    Student next;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.skills = new ArrayList<>();
+        this.next = null;
     }
 }
+class SkillNode {
+    String skill;
+    ArrayList<Student> students;
+    SkillNode left, right;
+
+    public SkillNode(String skill) {
+        this.skill = skill;
+        this.students = new ArrayList<>();
+        left = right = null;
+    }
+}
+
+class SkillManagementSystem {
+
+    private Student head;
+    private SkillNode root;
+
+    public void addStudent(int id, String name, List<String> skills) {
+
+        Student student = new Student(id, name);
+
+        for (String skill : skills) {
+            student.skills.add(skill);
+        }
+
+        if (head == null) {
+            head = student;
+        } else {
+            Student temp = head;
+
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+
+            temp.next = student;
+        }
+
+        for (String skill : skills) {
+            root = insertSkill(root, skill.toLowerCase(), student);
+        }
+
+        System.out.println("Student added successfully!");
+    }
+
+    private SkillNode insertSkill(
+            SkillNode node,
+            String skill,
+            Student student) {
+
+        if (node == null) {
+            SkillNode newNode = new SkillNode(skill);
+            newNode.students.add(student);
+            return newNode;
+        }
+
+        int compare = skill.compareTo(node.skill);
+
+        if (compare < 0) {
+            node.left = insertSkill(node.left, skill, student);
+        } else if (compare > 0) {
+            node.right = insertSkill(node.right, skill, student);
+        } else {
+            node.students.add(student);
+        }
+
+        return node;
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+
+    }
+}
+
+
